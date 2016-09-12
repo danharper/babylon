b308ec94c0f78b9a40f0d4066338f9be4218bd5b
Dan Harper <intouch@danharper.me>
2016-09-12 20:23:05 +0100

    fix: arrow on line after return type annotation is invalid

--- src/plugins/flow.js
+++ src/plugins/flow.js
@@ -1162,6 +1162,7 @@ export default function (instance) {
         let state = this.state.clone();
         try {
           let returnType = this.flowParseTypeAnnotation();
+          if (this.canInsertSemicolon()) this.unexpected();
           if (!this.match(tt.arrow)) this.unexpected();
           // assign after it is clear it is an arrow
           node.returnType = returnType;
